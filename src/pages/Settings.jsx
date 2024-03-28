bf02fa0ad5b3d8a58d4db6d6b18b3edb04a32fc8
Tr1d3nt <saudagha2@gmail.com>
2024-03-28 03:11:36 -0600

    feat: added confirm password

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -4,64 +4,96 @@ import { updatePassword } from "aws-amplify/auth";
 const Settings = () => {
   const [oldPassword, setOldPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [message, setMessage] = useState("");
+  const [messageType, setMessageType] = useState(""); // "success" or "error"
 
   async function handleUpdatePassword() {
+    if (newPassword !== confirmPassword) {
+      setMessage("New and confirm password's do not match.");
+      setMessageType("error");
+      return; // Exit early if passwords do not match
+    }
+
     try {
       await updatePassword({ oldPassword, newPassword });
       setMessage("Password successfully updated");
+      setMessageType("success");
       setOldPassword("");
       setNewPassword("");
+      setConfirmPassword("");
     } catch (err) {
       console.error(err);
       setMessage(err.message);
+      setMessageType("error");
     }
   }
 
   return (
-    <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-md">
-      <h1 className="text-xl font-semibold mb-4">Change Password</h1>
-      {message && (
-        <div className="mb-4 text-center text-sm font-medium text-green-600">
-          {message}
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md mx-auto mt-10 bg-white p-8 border border-gray-300 rounded-lg shadow-md">
+        <h1 className="text-xl font-semibold mb-4">Change Password</h1>
+        {message && (
+          <div
+            className={`mb-4 text-center text-sm font-medium ${
+              messageType === "error" ? "text-red-500" : "text-green-600"
+            }`}
+          >
+            {message}
+          </div>
+        )}
+        <div className="mb-4">
+          <label
+            htmlFor="oldPassword"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Old Password
+          </label>
+          <input
+            type="password"
+            id="oldPassword"
+            value={oldPassword}
+            onChange={(e) => setOldPassword(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
         </div>
-      )}
-      <div className="mb-4">
-        <label
-          htmlFor="oldPassword"
-          className="block text-sm font-medium text-gray-700"
+        <div className="mb-6">
+          <label
+            htmlFor="newPassword"
+            className="block text-sm font-medium text-gray-700"
+          >
+            New Password
+          </label>
+          <input
+            type="password"
+            id="newPassword"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="confirmPassword"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Confirm Password
+          </label>
+          <input
+            type="password"
+            id="confirmPassword"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+          />
+        </div>
+        <button
+          onClick={handleUpdatePassword}
+          className="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition duration-300"
         >
-          Old Password
-        </label>
-        <input
-          type="password"
-          id="oldPassword"
-          value={oldPassword}
-          onChange={(e) => setOldPassword(e.target.value)}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-        />
+          Change Password
+        </button>
       </div>
-      <div className="mb-6">
-        <label
-          htmlFor="newPassword"
-          className="block text-sm font-medium text-gray-700"
-        >
-          New Password
-        </label>
-        <input
-          type="password"
-          id="newPassword"
-          value={newPassword}
-          onChange={(e) => setNewPassword(e.target.value)}
-          className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-        />
-      </div>
-      <button
-        onClick={handleUpdatePassword}
-        className="w-full bg-blue-600 text-white p-2 rounded-lg hover:bg-blue-700 transition duration-300"
-      >
-        Change Password
-      </button>
     </div>
   );
 };
